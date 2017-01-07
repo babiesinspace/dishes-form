7c5c8e7d3b81ad4615bda5c11c8e7cde1a2003b7
[01;31m[K"name"[m[K: "Alexandra Cooper <[01;31m[K"email"[m[K: null>
2017-01-07 15:48:25 -0500

    Add simple form gem, new form, create route

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -3,4 +3,19 @@ class DishesController < ApplicationController
   def index
 
   end 
+
+  def new
+    @dish = Dish.new 
+  end 
+
+  def create
+    dish = Dish.create(dish_params)
+    redirect_to dishes_path
+  end 
+
+  private
+
+  def dish_params  
+    params.require(:dish).permit(:image, :description)
+  end 
 end
