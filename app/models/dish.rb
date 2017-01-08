79f3a2540f41f808b3d39256e1aa1178b283f3b9
[01;31m[K"name"[m[K: "Alexandra Cooper <[01;31m[K"email"[m[K: null>
2017-01-08 14:00:34 -0500

    trying to upload to s3

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,6 +1,9 @@
 class Dish < ApplicationRecord
   validates :image, :name, presence: true
 
-  has_attached_file :image, styles: { :medium => "640x" }
+  has_attached_file :image, styles: { 
+    thumb: '100x100>',
+    square: '200x200#',
+    :medium => "640x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
---
+++ config/initializers/s3.rb
@@ -0,0 +1,4 @@
+AWS::S3::Base.establish_connection!(
+ :access_key_id   => ENV['AKIAJ2BOBVHRL5K7H6YQ'],
+ :secret_access_key => ENV['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']
+)
