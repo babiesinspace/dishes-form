27a6ee9d60ff986702d5e47ddfaf63b1049a9ae3
[01;31m[K"name"[m[K: "Alexandra Cooper <[01;31m[K"email"[m[K: null>
2017-01-08 17:56:30 -0500

    heroku fuckery

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
 
  config.paperclip_defaults = {
   storage: :s3,
-  s3_host_name: ENV["AWS_S3_HOST_NAME"],
+  s3_host_name: 'http://dishesform.s3.amazonaws.com',
   s3_credentials: {
     bucket: ENV.fetch('S3_BUCKET_NAME'),
     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
