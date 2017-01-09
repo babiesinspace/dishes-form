8872b5c1891f6890b271999bf497383954c6b095
[01;31m[K"name"[m[K: "Alexandra Cooper <[01;31m[K"email"[m[K: null>
2017-01-08 21:05:09 -0500

    Give this gps coder a try

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,5 +1,6 @@
 class Dish < ApplicationRecord
   validates :image, :name, presence: true
+  after_post_process :save_latlong
 
   has_attached_file :image, styles: { 
     thumb: '100x100>',
@@ -7,4 +8,24 @@ class Dish < ApplicationRecord
     :medium => "640x" }
     
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+  private
+
+  def save_latlong
+    exif_data = MiniExiftool.new(image.queued_for_write[:original].path)
+    self.latitude = parse_latlong(exif_data['gpslatitude'])
+    self.longitude = parse_latlong(exif_data['gpslongitude'])
+  end
+
+  def parse_latlong(latlong)
+    return unless latlong
+    match, degrees, minutes, seconds, rotation = /(\d+) deg (\d+)' (.*)" (\w)/.match(latlong).to_a
+    calculate_latlong(degrees, minutes, seconds, rotation)
+  end
+
+  def calculate_latlong(degrees, minutes, seconds, rotation)
+    calculated_latlong = degrees.to_f + minutes.to_f/60 + seconds.to_f/3600
+    ['S', 'W'].include?(rotation) ? -calculated_latlong : calculated_latlong
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170109005420) do
+ActiveRecord::Schema.define(version: 20170109013546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20170109005420) do
     t.boolean  "special"
     t.string   "location"
     t.integer  "taste"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
 end
